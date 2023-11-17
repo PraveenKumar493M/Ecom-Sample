c98be6a249874e39e49925865f21f99db04751a4
PraveenKumar493M <120563826+PraveenKumar493M@users.noreply.github.com>
2023-11-17 19:03:13 +0530

    Update index.js

--- Ecom_sample/index.js
+++ Ecom_sample/index.js
@@ -3,6 +3,5 @@ function categoryFind(cat)
     localStorage.setItem("category",cat);
     window.location.href="./product.html";
 }
-
  // console.log(localStorage.setItem("category":cat))
 // document.getElementById("ecom").innerHTML
